6beee95e7479d63506ae85ea4c088468df1e317c
dennys1994 <dennys_1994@live.com>
2024-06-02 15:36:40 -0300

    Update db.php

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 $host = 'localhost';
 $db = 'ponto_digital_master';
 $user = 'root';
-$pass = 'Tecnonet-99';
+$pass = '********';
 $charset = 'utf8mb4';
 
 $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
